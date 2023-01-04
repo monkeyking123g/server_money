49053d10ad2f433cec8061e9b0890452870a360d
monkeyking123g <monkeyking123g>
2023-01-04 21:32:45 +0100

    update db

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 const db = mysql.createConnection({
-host: "185.166.188.115",
-port:65002,
+host: "185.166.188.103",
+port:3306,
 user: "u713542740_dimaipatii",
 password: "Xz9E8YthM2Z#N$v",
 database:"u713542740_Time",
