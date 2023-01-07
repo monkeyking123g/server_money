cb5e79292bde52ef180fde15ebd298598309555b
monkeyking123g <monkeyking123g>
2023-01-07 20:42:17 +0100

    Access-Control-Allow-Origin +++

--- index.js
+++ index.js
@@ -7,13 +7,6 @@ const multer = require("multer");
 const app = express();
 const PORT = 3002;
 
-
-// const corsOptions = {
-//   origin: "https://time-money-i34ctdexu-dimaipatii1994-gmailcom.vercel.app/",
-//   credentials: true, //access-control-allow-credentials:true
-//   optionSuccessStatus: 200,
-// };
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
@@ -21,6 +14,9 @@ app.use("/public", express.static("public"));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header("Access-Control-Allow-Headers","*");
+res.header('Access-Control-Allow-Credentials', true);
+res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   next();
 });
 
