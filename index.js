fbee22ca686d4ff791d05144341a440d50dd96a9
monkeyking123g <monkeyking123g>
2023-01-08 10:54:44 +0100

    add string

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = 3002;
 
 app.use(cors());
 app.use(express.json());
-// app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static("public"));
 
 app.use((req, res, next) => {
