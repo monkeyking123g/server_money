6f727fb4d0f7974852c146d79309b5808473ad53
monkeyking123g <monkeyking123g>
2023-01-09 20:55:22 +0100

    fix bag 2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const multer = require("multer");
 
 const app = express();
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
