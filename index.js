f4ea20a2ea7440ee7030b93e4d3a0f89d97bf04d
monkeyking123g <monkeyking123g>
2023-01-10 21:55:47 +0100

    cors setting

--- index.js
+++ index.js
@@ -12,13 +12,19 @@ app.use(express.json());
 // app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static("public"));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   res.header("Access-Control-Allow-Headers","*");
-// res.header('Access-Control-Allow-Credentials', true);
-// res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-//   next();
-// });
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 
 var fileFilter = function (req, file, cb) {
   if (!file) {
