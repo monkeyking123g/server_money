a7a70ffd30e8e268104e658ccaa72c29cbb4e7ec
monkeyking123g <monkeyking123g>
2023-01-12 16:02:11 +0100

    sql normal

--- config/db.js
+++ config/db.js
@@ -1,23 +1,12 @@
-// const mysql = require('mysql')
-// const db = mysql.createConnection({
-// host: "185.166.188.103",
-// // port:3306,
-// user: "u713542740_dimaipatii",
-// password: "Xz9E8YthM2Z#N$v",
-// database:"u713542740_Time",
-// connectionLimit: 10 
-// })
-
-// module.exports = db;
-
-const mysql = require('mysql')
-const db = mysql.createConnection({
-host: "containers-us-west-182.railway.app",
-// port: 6229,
-user: "root",
-password: "6HYgKnxOv3oDJbRL8apl",
-database:"railway",
-connectionLimit: 10 
-})
+ const mysql = require('mysql')
+ const db = mysql.createConnection({
+ host: "185.166.188.103",
+ // port:3306,
+ user: "u713542740_dimaipatii",
+ password: "Xz9E8YthM2Z#N$v",
+ database:"u713542740_Time",
+ connectionLimit: 10 
+ })
 
 module.exports = db;
+
