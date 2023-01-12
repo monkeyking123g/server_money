e0e831c1e425d41833a7b1a227fde7269d2fabdd
monkeyking123g <monkeyking123g>
2023-01-12 15:20:49 +0100

    change db

--- config/db.js
+++ config/db.js
@@ -1,11 +1,22 @@
+// const mysql = require('mysql')
+// const db = mysql.createConnection({
+// host: "185.166.188.103",
+// // port:3306,
+// user: "u713542740_dimaipatii",
+// password: "Xz9E8YthM2Z#N$v",
+// database:"u713542740_Time",
+// connectionLimit: 10 
+// })
+
+// module.exports = db;
+
 const mysql = require('mysql')
 const db = mysql.createConnection({
-host: "185.166.188.103",
-// port:3306,
-user: "u713542740_dimaipatii",
-password: "Xz9E8YthM2Z#N$v",
-database:"u713542740_Time",
-connectionLimit: 10 
+host: "containers-us-west-63.railway.app",
+port: 6229,
+user: "root",
+password: "f8WvfTI9xB4wiPVGVBYF",
+database:"railway",
 })
 
 module.exports = db;
