d02a6402e867853187a6e6e2246ccd35feb90068
monkeyking123g <monkeyking123g>
2023-01-12 14:21:07 +0100

    cros fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/public", express.static("public"));
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://time-money.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', "*");
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
